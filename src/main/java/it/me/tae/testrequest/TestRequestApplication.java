a5ceb405da2d21057609ecab6e24dd660f3acdb0
piya_l <piya.tae@gmail.com>
2018-02-26 16:13:38 +0700

    Initial project

---
+++ src/main/java/it/me/tae/testrequest/TestRequestApplication.java
@@ -0,0 +1,12 @@
+package it.me.tae.testrequest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestRequestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestRequestApplication.class, args);
+	}
+}
---
+++ src/test/java/it/me/tae/testrequest/TestRequestApplicationTests.java
@@ -0,0 +1,16 @@
+package it.me.tae.testrequest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TestRequestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
